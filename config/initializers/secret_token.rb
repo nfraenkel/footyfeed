fed464a8f4882db7094e8ffb13756e8f92848630
Nathan Fraenkel <fraenkel@seas.upenn.edu>
2013-05-01 17:48:52 -0400

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Footyfeed::Application.config.secret_token = 'f23cf2f551e3c46fbe10b24a1446dd634560f1112ff6cef8a397ddd5104252e26e57440a0ac685c169820edf5d3575b42f6fcd358b8e7fec0ae76cdc0eeb5936'
