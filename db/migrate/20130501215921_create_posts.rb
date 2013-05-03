6e78e21461e93d827445d3a73b69430a20b9af68
Nathan Fraenkel <fraenkel@seas.upenn.edu>
2013-05-03 19:57:55 -0400

    idk

--- db/migrate/20130501215921_create_posts.rb
+++ db/migrate/20130501215921_create_posts.rb
@@ -2,6 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :title
+      t.string :imageURL
 
       t.timestamps
     end
--- db/migrate/20130503234814_add_time_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddTimeToPosts < ActiveRecord::Migration
-  def change
-    add_column :posts, :time, :datetime
-  end
-end
