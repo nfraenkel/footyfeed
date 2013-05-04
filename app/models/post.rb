d48b4d06fd8468751e3c3b658519b26dd1a7ecd3
Nathan Fraenkel <fraenkel@seas.upenn.edu>
2013-05-04 13:42:28 -0400

    giving up. just using user emails for now

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,11 @@ class Post < ActiveRecord::Base
   belongs_to :user
   attr_accessible :description, :image, :time, :title, :user_id
 
-  def user_name
-    user.first if user
+  #def user_name
+  #  user.first + " " + user.last if user
+  #end
+
+  def user_email
+    user.email if user
   end
 end
